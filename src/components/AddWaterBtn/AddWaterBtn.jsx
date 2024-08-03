5af81e58597e9532056452eb65329a1b82eb6b07
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-08-03 18:13:38 +0300

    add water Modal

--- src/components/AddWaterBtn/AddWaterBtn.jsx
+++ src/components/AddWaterBtn/AddWaterBtn.jsx
@@ -1,13 +1,35 @@
+import { useState } from 'react';
 import { FaPlus } from 'react-icons/fa6';
+import WaterModal from '../WaterModal/WaterModal';
 
 const AddWaterBtn = ({ containerClassName, buttonClassName, iconClassName }) => {
+  const [showWaterModal, setShowWaterModal] = useState(false);
+  const [operationType, setOperationType] = useState('add');
+
+  const onOpenWaterModal = (type) => {
+    setOperationType(type);
+    setShowWaterModal(true);
+  };
+
+  const onCloseWaterModal = () => {
+    setShowWaterModal(false);
+  };
+
   return (
     <div className={containerClassName}>
-      <button type="button" className={buttonClassName} onClick={() => { console.log('Open Water modal') }}>
-      <FaPlus className={iconClassName} />
-      Add Water</button>
+      <button type="button" className={buttonClassName} onClick={() => onOpenWaterModal('add')}>
+        <FaPlus className={iconClassName} />
+        Add Water
+      </button>
+      <WaterModal
+        waterModalOpen={showWaterModal}
+        closeWaterModal={onCloseWaterModal}
+        operationType={operationType}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AddWaterBtn
+export default AddWaterBtn;
+
+// приймає у вигляді пропсів стилі для кнопки в WaterMainInfo, і для кнопки в DailyInfo
