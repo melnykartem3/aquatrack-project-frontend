3073cf00aa953a7eedefdfeed91d431b7ebae2b6
Step004 <ctepan.filep9@gmail.com>
2024-08-03 13:14:35 +0300

    add markup for calendar mobile

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,13 +1,21 @@
-import CalendarItem from "../CalendarItem/CalendarItem"
+import CalendarItem from '../CalendarItem/CalendarItem';
+import css from './Calendar.module.css';
+const CalendarNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 
 const Calendar = () => {
-    return (
-      //тут рендериться список днів через map
-    <>
-        <div>Calendar</div>
-        <CalendarItem/>
-    </>
-  )
-}
+  return (
+    <div className={css.calendarContainer}>
+      <div className={css.calendarHeader}>
+        <span>Month</span>
+        <span>April, 2024</span>
+      </div>
+      <div className={css.calendarList}>
+        {CalendarNumbers.map(item => (
+          <CalendarItem key={item} number={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Calendar
+export default Calendar;
--- src/components/CalendarItem/CalendarItem.jsx
+++ src/components/CalendarItem/CalendarItem.jsx
@@ -1,8 +1,12 @@
+import css from './CalendarItem.module.css';
 
-const CalendarItem = () => {
+const CalendarItem = ({ number }) => {
   return (
-    <div>CalendarItem</div>
-  )
+    <div className={css.calendarItem}>
+      <button className={css.button}>{number}</button>
+      <div className={css.itemPercentage}>100%</div>
+    </div>
+  );
 }
 
 export default CalendarItem
