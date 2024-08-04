73d10cf914a2aa80d919e72318f6afa560781ea9
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-08-05 02:47:27 +0300

    fix modals styles

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,34 +1,25 @@
 import ModalReact from 'react-modal';
 import css from '../Modal/Modal.module.css';
-import clsx from 'clsx';
+// import clsx from 'clsx';
 import { IoClose } from 'react-icons/io5';
 
 const Modal = ({ modalIsOpen, closeModal, children }) => {
   const customStyles = {
     overlay: {
       backgroundColor: 'rgba(47, 47, 47, 0.6)',
+      
     },
   };
-
-  const modalClassName = clsx(
-    css.modal,
-    css.reactModal_Content,
-  );
-
-  const overlayClassName = clsx(
-    css.ReactModal__Overlay,
-    modalIsOpen && css.isOpen,
-  );
 
   return (
     <ModalReact
       isOpen={modalIsOpen}
-      overlayClassName={overlayClassName}
-      className={modalClassName}
       closeTimeoutMS={300}
       onRequestClose={closeModal}
       ariaHideApp={false}
       style={customStyles}
+      className={css.reactModal_Content}
+      bodyOpenClassName={css.ReactModal__Body} 
     >
       <button className={css.closeBtn} onClick={closeModal}>
         <IoClose className={css.closeIcon} />
