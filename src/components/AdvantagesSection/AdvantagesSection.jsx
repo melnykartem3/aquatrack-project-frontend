0130c94c324e6628eb85c776ffe5f92d139e16c7
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2024-08-05 23:03:06 +0300

    fixed styles, icons for retina

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -1,37 +1,40 @@
 import css from './AdvantagesSection.module.css';
+import user1 from '../../images/homePage/user-1.png';
+import user1_2x from '../../images/homePage/user-1@2x.png';
+import user2 from '../../images/homePage/user-2.png';
+import user2_2x from '../../images/homePage/user-2@2x.png';
+import user3 from '../../images/homePage/user-3.png';
+import user3_2x from '../../images/homePage/user-3@2x.png';
+import icon from '../../images/homePage/icon.svg';
 
 const AdvantagesSection = () => {
   return (
     <section className={css.advantagesSection}>
       <div className={css.customers}>
-        <img
-          src="/src/images/homePage/user-1.png"
-          alt="user-1.png"
-          className={css.icon}
-        />
-        <img
-          src="/src/images/homePage/user-2.png"
-          alt="user-2.png"
-          className={css.icon}
-        />
-        <img
-          src="/src/images/homePage/user-3.png"
-          alt="user-3.png"
-          className={css.icon}
-        />
+        <picture>
+          <source srcSet={`${user1_2x} 2x, ${user1} 1x`} />
+          <img src={user1} alt="User 1" className={css.icon} />
+        </picture>
+        <picture>
+          <source srcSet={`${user2_2x} 2x, ${user2} 1x`} />
+          <img src={user2} alt="User 2" className={css.icon} />
+        </picture>
+        <picture>
+          <source srcSet={`${user3_2x} 2x, ${user3} 1x`} />
+          <img src={user3} alt="User 3" className={css.icon} />
+        </picture>
         <p>
           Our <span className={css.span}>happy</span> customers
         </p>
       </div>
 
-      <div className={css.advantagesListContainer}>
+      <div>
         <ul className={css.advantagesList}>
           <li className={css.li_1}>
-            <img
-              src="/src/images/homePage/icon.svg"
-              alt="icon"
-              className={css.dotIcon}
-            />
+            <picture>
+              <source srcSet={`${icon} 1x`} />
+              <img src={icon} alt="Icon" className={css.dotIcon} />
+            </picture>
             Habit drive
           </li>
           <li className={css.li_2}>View statistics</li>
