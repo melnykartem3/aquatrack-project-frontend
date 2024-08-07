34c5785109baa390b742c8039048252e947e858c
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-08-07 14:50:07 +0300

    .

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,48 +1,52 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { instance, setToken, clearToken } from '../../utils/axios';
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { instance } from "../../utils/axios";
 
-export const registerUser = createAsyncThunk(
-  'auth/register',
-  async (formData, thunkApi) => {
+
+
+export const register = createAsyncThunk(
+  "auth/register",
+    async (formData, thunkApi) => {
     try {
-      const response = await instance.post('/users/register', formData);
-
+        const response = await instance.post("/users/register", formData);
+      
       return response.data;
     } catch (error) {
       return thunkApi.rejectWithValue(error.message);
     }
-  },
+  }
 );
 
 export const login = createAsyncThunk(
   'auth/login',
-  async (credentials, thunkAPI) => {
+  async (loginData, thunkAPI) => {
     try {
-      const response = await instance.post('/auth/login', credentials);
-      const { accessToken } = response.data;
-      setToken(accessToken);
-      return response.data;
+        const response = await instance.post('/users/login', loginData);
+  
+        return response.data;
+        
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data || error.message);
+      return thunkAPI.rejectWithValue(error.message);
     }
-  },
+  }
 );
 
-export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
-  try {
-    await instance.post('/auth/logout');
-    clearToken();
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data || error.message);
+export const logout = createAsyncThunk(
+  'auth/logout',
+  async (_, thunkAPI) => {
+    try {
+        await instance.post('/users/logout');
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-});
+);
 
 // export const refreshUser = createAsyncThunk(
 //   "auth/refresh",
 //   async (_, thunkApi) => {
 //     const state = thunkApi.getState();
 //     const token = state.auth.token;
-
+    
 //     if (token === null) {
 //       return thunkApi.rejectWithValue('Unable to fetch user');
 //     }
@@ -54,3 +58,4 @@ export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
 //     }
 //   }
 // );
+
