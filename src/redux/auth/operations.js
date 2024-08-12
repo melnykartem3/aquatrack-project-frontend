ad294709cace46f4686ebeb30b7d78b9ab7bd596
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-08-12 13:33:50 +0300

    error

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import {instance} from '../../utils/axios';
+import { instance } from '../../utils/axios';
+import toast from 'react-hot-toast';
 export const registerUser = createAsyncThunk(
   'auth/signup',
   async (formData, thunkAPI) => {
@@ -7,22 +8,28 @@ export const registerUser = createAsyncThunk(
       const response = await instance.post('/auth/signup', formData);
       return response.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+      const errorMessage = error.response?.data?.data?.message || error.message;
+      toast.error(errorMessage);
+      return thunkAPI.rejectWithValue(errorMessage);
     }
   },
 );
 export const login = createAsyncThunk(
-
   'auth/login',
   async (loginData, thunkAPI) => {
     try {
       const response = await instance.post('/auth/signin', loginData);
       return response.data.data;
     } catch (error) {
-      return thunkAPI.rejectWithValue(error.message);
+      const errorMessage = error.response?.data?.data?.message || error.message;
+
+      // console.log('Error message from backend:', errorMessage);
+      toast.error(errorMessage);
+      return thunkAPI.rejectWithValue(errorMessage);
     }
   },
 );
+
 export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
     await instance.post('/auth/logout');
@@ -31,38 +38,37 @@ export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   }
 });
 
-export const refresh = createAsyncThunk(
-  'auth/refresh',
-  async (_, thunkApi) => {
-    try {
-      const { data } = await instance.post('/auth/refresh');
-      return data.data;
-    } catch (error) {
+export const refresh = createAsyncThunk('auth/refresh', async (_, thunkApi) => {
+  try {
+    const { data } = await instance.post('/auth/refresh');
+    return data.data;
+  } catch (error) {
     return thunkApi.rejectWithValue(error.message);
   }
-  }
-);
+});
 
-export const getUser = createAsyncThunk('auth/current',
-  async (_, thunkAPI) => {
+export const getUser = createAsyncThunk('auth/current', async (_, thunkAPI) => {
   try {
     const { data } = await instance.get('/auth/current');
     return data;
   } catch (e) {
     return thunkAPI.rejectWithValue(e.message);
   }
-  });
-
+});
 
 export const updateUser = createAsyncThunk(
   'auth/update',
   async ({ userId, ...formData }, thunkAPI) => {
     try {
-      const { data } = await instance.patch(`/auth/update/${userId}`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
+      const { data } = await instance.patch(
+        `/auth/update/${userId}`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
         },
-      });
+      );
       console.log(data);
       return data;
     } catch (e) {
