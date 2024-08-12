426477f53e0e89c19547c7354ed3b7e4164987fb
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-08-13 01:22:34 +0300

    add import i18n

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'modern-normalize'
-import App from './components/App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { persistor, store } from './redux/store.js'
-import { BrowserRouter } from 'react-router-dom'
-import { PersistGate } from 'redux-persist/integration/react'
-import { HelmetProvider } from 'react-helmet-async'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'modern-normalize';
+import App from './components/App.jsx';
+import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { HelmetProvider } from 'react-helmet-async';
 import './i18n.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider><Provider store={store}>
-    <PersistGate persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
+      </Provider>
     </HelmetProvider>
-    
   </React.StrictMode>,
-)
+);
