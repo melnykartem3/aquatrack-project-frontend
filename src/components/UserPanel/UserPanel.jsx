1b6e30cf63c8573d7e4c4c42002d42add2886d5f
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-08-13 17:51:47 +0300

    finished userPanel

--- src/components/UserPanel/UserPanel.jsx
+++ src/components/UserPanel/UserPanel.jsx
@@ -1,13 +1,46 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from '../../redux/auth/selectors.js';
+import { getUser } from '../../redux/auth/operations.js';
+
 import UserBar from '../UserBar/UserBar';
 import css from './UserPanel.module.css';
 
 const UserPanel = ({ setOpenSetting }) => {
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
+
+  useEffect(() => {
+    dispatch(getUser());
+  }, [dispatch]);
+
+  const getDisplayName = (name, email) => {
+    if (name && name !== 'User') {
+      return name;
+    } else if (email) {
+      return email.split('@')[0];
+    } else {
+      return 'User';
+    }
+  };
+
+  const email = user?.email;
+  const name = user?.name;
+  const displayName = getDisplayName(name, email);
+  const shortDisplayName = displayName.substring(0, 5);
+  const avatarURL = user?.avatar;
+
   return (
     <div className={css.panelWrapper}>
       <h2 className={css.title}>
-        Hello<span className={css.secondaryText}>, User!</span>
+        Hello
+        <span className={css.secondaryText}>, {displayName}!</span>
       </h2>
-      <UserBar setOpenSetting={setOpenSetting} />
+      <UserBar
+        setOpenSetting={setOpenSetting}
+        shortDisplayName={shortDisplayName}
+        avatarURL={avatarURL}
+      />
     </div>
   );
 };
