967f0ea6623dcbd32ef089131dde22902fd6036e
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-08-13 23:17:50 +0300

    not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,11 @@
 import css from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
-  return <div className={css.no_page}>Wooops, page is not found!</div>;
+  return <div className={css.notFoundPager}>
+    <div className={css.notFoundPage_wrapper}>
+      <p className={css.notFoundPage_text}>Wooops, page is not found!</p>
+    </div>
+  </div>;
 };
 
 export default NotFoundPage;
