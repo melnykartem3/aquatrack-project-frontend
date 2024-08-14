5571e0aca7aa50318338e92596ced31971acfc07
Artem Melnik <melnikartem@vesele-gimnaz.com>
2024-08-14 13:29:53 +0300

    add translation for not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,16 @@
+import { useTranslation } from 'react-i18next';
+
 import css from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
-  return <div className={css.notFoundPager}>
-    <div className={css.notFoundPage_wrapper}>
-      <p className={css.notFoundPage_text}>Wooops, page is not found!</p>
+  const { t } = useTranslation();
+  return (
+    <div className={css.notFoundPager}>
+      <div className={css.notFoundPage_wrapper}>
+        <p className={css.notFoundPage_text}>{t('notFoundPage')}</p>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default NotFoundPage;
